3db063d7ef5b44b9fbf4b878858742bead7de324
Ben Dreyer <38509839+bdreyerr@users.noreply.github.com>
2019-03-02 13:07:43 -0800

    Google Maps Initial Commit

--- Hackathon2019/MainActivity.cs
+++ Hackathon2019/MainActivity.cs
@@ -3,17 +3,30 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Gms.Maps;
 
 namespace Hackathon2019
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
     {
+        GoogleMap mainMap;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
         }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mainMap = googleMap;
+        }
+
+       
     }
 }
